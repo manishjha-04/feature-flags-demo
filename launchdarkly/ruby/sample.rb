fa0f23acb82b9766b22e1edd045d4ea5d89b9cb9
Gitar <noreply@gitar.co>
2024-07-16 21:14:27 +0000

    [Gitar] Cleaning up Ruby files

--- launchdarkly/ruby/sample.rb
+++ launchdarkly/ruby/sample.rb
@@ -17,13 +17,8 @@
 module WelcomeHelper
     def check_feature_flags(user, ld_client)
         # If dark mode is enabled, check for high contrast mode
-        if ld_client.variation("is-dark-mode-enabled", user, false)
-            # Check and report the 'enable-high-contrast' feature flag for the specified user
-            high_contrast = ld_client.variation("enable-high-contrast", user, false)
-            puts "Dark mode - high contrast mode is #{high_contrast}"
-        else
-            puts "Light mode!"
-        end
+        high_contrast = ld_client.variation("enable-high-contrast", user, false)
+          puts "Dark mode - high contrast mode is #{high_contrast}"
     end
     
     def enabled_status(flag_value)
