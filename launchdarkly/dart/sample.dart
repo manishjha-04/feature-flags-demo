62e5158add9efb2a4228779b79f07492cd469eac
Gitar <noreply@gitar.co>
2024-07-26 21:21:26 +0000

    [Gitar] Updating Dart files

--- launchdarkly/dart/sample.dart
+++ launchdarkly/dart/sample.dart
@@ -13,20 +13,8 @@ class SampleDart {
 
     await ldClient.start(user);
 
-    bool flagValue = await ldClient.boolVariation("is-dark-mode-enabled", false);
-
-    if (flagValue) {
-      // The code to run if the feature is on
-      System.out.println("Dark mode!");
-    } else {
-      // The code to run if the feature is off
-      bool otherFlag = await ldClient.boolVariation("is-ameyas-weird-blue-theme", false);
-      if (otherFlag) {
-        System.out.println("Why? Just... why?");
-      } else {
-        System.out.println("Light mode it is, then!");
-      }
-    }
+    // The code to run if the feature is on
+    System.out.println("Dark mode!");
 
     await ldClient.close();
   }
